2f167eee8cd22a4e4588886b8c55bca799eeed72
arya-19 <arya.karambelkar@gmail.com>
2022-04-27 22:49:08 +0530

    Final changes

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 # Important imports
-from app.main import app
+from app import app
 from flask import request, render_template
 import cv2
 import numpy as np
@@ -10,18 +10,21 @@ import string
 import pytesseract
 import pyttsx3
 from gtts import gTTS
+import keyboard
+import time
+
+#defining global vars
+
+finaltext = "" 
+new_string1 = ""
+full_filename = ""
+new_string = ""
 
 # Adding path to config
 app.config['INITIAL_FILE_UPLOADS'] = 'app/static/uploads' #check the size of img and ration bw text and image
 
-new_string1 = ""
-# @app.route('/')
-# def index():
-#     register_form = RegisterForm()
-#     login_form = LoginForm()
-#     return render_template('index.html', register_form=register_form, login_form=login_form)
 
-
+#routing to textractor page
 @app.route('/', methods=['GET', 'POST'])
 def textractor():
     return render_template('textractor.html')
@@ -33,6 +36,7 @@ def index():
 
 	# Execute if request is get
 	if request.method == "GET":
+		global full_filename
 		full_filename =  'images/white_bg.jpg' #taking dummy image from image folder 
 		return render_template("index.html", full_filename = full_filename) #bascically displaying initial dummy image on flask interface and code assigns LHS "full_filename" it to the same name in the index.html file and pastes it there
 
@@ -55,65 +59,62 @@ def index():
 		name = ''.join(random.choice(letters) for i in range(10)) + '.png'
 		full_filename =  'uploads/' + name
 
-		pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
-
+		pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 		# Extracting text from image
 		custom_config = r'-l eng --oem 3 --psm 6' #language is selected as eng, oem specifies OCR engine mode, 3 is default, PSM is page segmentation mode, 6 is single uniform block of text.
 		text = pytesseract.image_to_string(image,config=custom_config)
 
-		# Remove symbol if any
-		# characters_to_remove = "!()@—*“>+-/,'|£#%$&^_~" 
+		global new_string
 		new_string = text
-		new_string1 = text
-		# for character in characters_to_remove: 
-		# 	new_string = new_string.replace(character, "")
-
-		#text to speech part--------------------------------------------------------------------
-		# f = open("output.txt", "w")
-		# f.write(new_string1)
-
+		
+		global finaltext
 
 		# Converting string into list to dislay extracted text in seperate line
 		new_string = new_string.split("\n") #detects new line in image and displays text accordingly on a new line
+		finaltext = ' '.join(new_string)
+		print(finaltext)
 
 		# Saving image to display in html
 		img = Image.fromarray(image_arr, 'RGB') 
 		img.save(os.path.join(app.config['INITIAL_FILE_UPLOADS'], name))
+
+		# text_to_speech(text, "Male")
 
 		# Returning template, filename, extracted text
 		return render_template('index.html', full_filename = full_filename, text = new_string)
 
 
 def text_to_speech(text, gender):
+	
     voice_dict = {'Male': 0, 'Female': 1}
     code = voice_dict[gender]
 
     engine = pyttsx3.init()
 
-    # Setting up voice rate
+	# Setting up voice rate
     engine.setProperty('rate', 125)
-
-    # Setting up volume level  between 0 and 1
-    engine.setProperty('volume', 0.8)
 
     # Change voices: 0 for male and 1 for female
     voices = engine.getProperty('voices')
     engine.setProperty('voice', voices[code].id)
 
-    engine.say(text)
-    engine.runAndWait()
+    try:
+        engine.say(text)
+        if keyboard.is_pressed("esc"):
+            engine.stop()
+        engine.runAndWait()
+        engine.stop()        
+    except:
+        return render_template('index.html', full_filename = full_filename, text = new_string)
+
 
 @app.route('/voice', methods=['POST', 'GET'])
 def voice():
-    if request.method == 'POST':
-		# read_string = open("output.txt", "r")
-        text = "hello"
-        gender = request.form['voices']
-        text_to_speech(text, "Male")
-        return render_template('voice.html')
-    else:
-        return render_template('voice.html')
-
+	text = finaltext
+	# gender = request.form['voices']
+	# time.sleep(10)
+	text_to_speech(text, "Male")
+	return render_template('index.html', full_filename = full_filename, text = new_string)
 
 
 # Main function
